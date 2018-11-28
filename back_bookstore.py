88b81d11c1f9d1e16b92e56079a9ba28a956b40e
Nicolas Nosenzo <niconosenzo@gmail.com>
2018-11-28 15:29:18 -0300

    gpdr

--- back_bookstore.py
+++ back_bookstore.py
@@ -56,17 +56,7 @@ def test():
 
 def main():
     init_table()
-    insert_data("the earth","Melani Martinez",2018,2452443534)
-    # for r in print_data():
-    #     print(r)
-    # for r in search(isbn="2452443534"):
-    #     print(r)
-    #delete_data(2)
-    # for r in print_data():
-    #     print(r)
-    # update_data(1, 'the sea', 'Nicolas Nosenzo', 2000, 2452443534)
-    # for r in print_data():
-    #     print(r)
+    insert_data("the earth","Melani tinez",2018,2452443534)
 
 
 if __name__ == '__main__':
